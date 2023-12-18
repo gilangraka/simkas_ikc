51ac10b18d58d53cd876914821e654a7ece2765f
gilangraka <gilangrakar@gmail.com>
2023-12-18 14:07:29 +0700

    Bisa!

--- pages/dashboard_admin/fungsi_tambah_kas.php
+++ pages/dashboard_admin/fungsi_tambah_kas.php
@@ -2,12 +2,13 @@
     require "../config.php";
     $db = new Database();
     
-    $deskripsi = $_POST['deskripsi'];
-    $nominal = $_POST['nominal'];
-    $json = '{"33422206":0, "33422208":0, "33422212":0, "33422215":0, "33422221":0, }';
-    json_encode($json);
-    $tanggal_masuk = date("Y-m-d");
-    $res = $db->jalankan_query("INSERT INTO data_kas_masuk_keluar VALUES('', '$tanggal_masuk', '$deskripsi', '$nominal', '$json', '0')");
-    var_dump($res);
-    
+    if(isset($_POST['submit'])) {
+        $deskripsi = $_POST['deskripsi'];
+        $nominal = $_POST['nominal'];
+        $json = '{"33422206":0, "33422208":0, "33422212":0, "33422215":0, "33422221":0, }';
+        json_encode($json);
+        $tanggal_masuk = date("Y-m-d");
+        $db->jalankan_query("INSERT INTO data_kas_masuk_keluar VALUES(NULL, '$tanggal_masuk', '$deskripsi', '$nominal', '{\"33422206\":0,\"33422208\":1,\"33422212\":0,\"33422215\":0,\"33422221\":0}', '0')");
+        header("location: dashboard.php");
+    }
 ?>
